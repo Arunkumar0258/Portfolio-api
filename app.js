b15217dcca3e9d8d523ec36469650f7a43480cfb
Arun Kumar <everythinghere007@gmail.com>
2017-05-17 00:48:57 +0530

    mongoose code update

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+mongoose.Promise = global.Promise;
+mongoose.connect('localhost:27017');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
         res.send('hello world')
         console.log('this is awesome')
+})
+
+app.use(function(req,res,next) {
+        var err = new Error('Not Found')
+        err.status = 404;
+        next(err);
+})
+
+app.use(function(err,req,res) {
+        res.status(err.status | 500).send();
 })
 
 app.listen(3000, function() {
