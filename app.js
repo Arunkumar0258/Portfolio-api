52dd7fac2c03136132a7760ad312d54ad4f7127c
Arun Kumar <everythinghere007@gmail.com>
2017-05-16 21:47:08 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const express = require('express')
+const app = express();
+
+app.get('/', function(req, res) {
+        res.send('hello world')
+})
+
+app.listen(3000, function() {
+        console.log('server is running in port 3000');
+})
+
+
